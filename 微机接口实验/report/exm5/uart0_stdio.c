e6e6b1606bb4785fc1b951e6baf37fa3c30766c1
richsoap <1500012956@pku.edu.cn>
2018-05-09 17:19:57 +0800

    jhg

--- 微机接口实验/report/exm5/uart0_stdio.c
+++ 微机接口实验/report/exm5/uart0_stdio.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <C8051F020.h>
 #include <stdio.h>
 #include "../../includes/communicate.h"
@@ -18,7 +17,7 @@ int main() {
     if(scanf("%d %c %d",&a,&b,&c)) {
       switch(b) {
         case '+' :
-		result = a+c; 
+		result = a+c;
         break;
         case '-':
 		result = a-c;
@@ -36,37 +35,3 @@ int main() {
     }
   }
 }
-=======
-#include <C8051F020.h>
-#include <stdio.h>
-#include "communicate.h"
-#include "time.h"
-
-int main() {
-  int a,b,c,result;
-  WDTCN = 0xDE;
-  WDTCN = 0xAD;
-  sysclk_init();
-  uart0_port_init();
-  uart0_init();
-  TI0 = 1;
-  printf("Enter number cacu number\n");
-  while(1) {
-    if(scanf("%b %b %b",&a,&b,&c) == 6) {
-      switch(b) {
-        case '+' : printf("%b\n",a+c);
-        break;
-        case '-':
-        printf("%b\n",a-c);
-        break;
-        case '*':
-        printf("%b\n",a*c);
-        break;
-        case '/':
-        printf("%b\n",a/c);
-        break;
-      }
-    }
-  }
-}
->>>>>>> 39c049beb19b1056eb1ee906be3afdf5f37b94bf
